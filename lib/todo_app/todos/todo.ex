de5cc85a8a349c09f8c0a346a767ee0b55b198e4
John-77 <john.vas131@gmail.com>
2025-08-26 11:39:36 -0400

    init + login

---
+++ lib/todo_app/todos/todo.ex
@@ -0,0 +1,19 @@
+defmodule TodoApp.Todos.Todo do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "todos" do
+    field :done, :boolean, default: false
+    field :title, :string
+    belongs_to :user, TodoApp.Accounts.User
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(todo, attrs) do
+    todo
+    |> cast(attrs, [:title, :done])
+    |> validate_required([:title, :done])
+  end
+end
